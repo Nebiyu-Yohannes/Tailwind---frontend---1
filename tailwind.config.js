b3bd02baf9ca9072adc1dc55513c06fcb1793820
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-06-23 14:56:23 +0300

    .365

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ module.exports = {
       /* our custom styles go here */
       fontFamily: {
         body: ['Nunito']
-      }     
+      },
+      
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      },
 
     },
 
