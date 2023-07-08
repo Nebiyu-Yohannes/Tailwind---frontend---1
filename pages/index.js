f4433ad9fc8a9d42e50382eaf2ba0882cf0b63d5
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-07-08 18:45:20 +0300

    .123

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ const menu_icon = document.querySelector('#menu-icon-container');
 
 const menu = document.querySelector('#menu-list');
 
-const open_menu = ()=>{
+/*const open_menu = ()=>{
     if (menu.classList.contains('hidden')){
         menu.classList.remove('hidden');
     } else {
         menu.classList.add('hidden');
     }
 }
-
+*/
 
 
 menu_icon.addEventListener('click', () => {
