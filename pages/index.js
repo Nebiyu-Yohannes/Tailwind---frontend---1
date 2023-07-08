9f002657831ff88da4741d2e49a151e1e693a64a
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-07-08 20:00:36 +0300

    .123

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ const menu_icon = document.querySelector('#menu-icon-container');
 const menu = document.querySelector('#menu-list');
 
 
+
 // this is the declaration of a menu_opening function 
 // that adds and removes the class hidden, based on it being there already
 
@@ -18,19 +19,7 @@ const open_menu = ()=>{
     }
 }
 
-
-// this is also another way to implement it 
-// by directly creating an arrow function in the event listener method for the click event 
-
-/*menu_icon.addEventListener('click', () => {
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-}
-);
-*/
-
-
+// now we call the open_menu function when the menu icon is clicked
 menu_icon.addEventListener('click', open_menu);
+
+
