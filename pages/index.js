6c1709ba50e8ba7c03b11b195157806b683cddee
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-07-08 19:34:23 +0300

    .23

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,19 @@ const menu_icon = document.querySelector('#menu-icon-container');
 const menu = document.querySelector('#menu-list');
 
 
-// this is the 
+// this is the declaration of an open
 
-/*const open_menu = ()=>{
+const open_menu = ()=>{
     if (menu.classList.contains('hidden')){
         menu.classList.remove('hidden');
     } else {
         menu.classList.add('hidden');
     }
 }
-*/
 
 
-menu_icon.addEventListener('click', () => {
+
+/*menu_icon.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
     } else {
@@ -25,5 +25,5 @@ menu_icon.addEventListener('click', () => {
     }
 }
 );
-
+*/
 menu_icon.addEventListener('click', open_menu);
